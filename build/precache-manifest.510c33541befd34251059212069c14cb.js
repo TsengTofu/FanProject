2a9226e28e445a15e5b6cf80473af2ba62f49b3d
Tofu <tsengbatty@gmail.com>
2019-05-06 04:19:23 +0800

    Can exchange but not detail_0506

---
+++ build/precache-manifest.510c33541befd34251059212069c14cb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "19e8c95f2cda8abda0f1",
+    "url": "/static/css/main.9e543c58.chunk.css"
+  },
+  {
+    "revision": "19e8c95f2cda8abda0f1",
+    "url": "/static/js/main.f6ba4a89.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b4db2eb3dbc5f49f80c6",
+    "url": "/static/js/2.a097469c.chunk.js"
+  },
+  {
+    "revision": "54c6d733f2774af4bcc4471b5ce877de",
+    "url": "/static/media/logout.54c6d733.svg"
+  },
+  {
+    "revision": "26258da264a9dbdfea88f674f1386ce6",
+    "url": "/static/media/member.26258da2.svg"
+  },
+  {
+    "revision": "0e79ead4b66a45f62e8ae1052102ca11",
+    "url": "/static/media/eastwood-page-under-construction.0e79ead4.png"
+  },
+  {
+    "revision": "950b16eecf22b6628a2f38565fd7b843",
+    "url": "/static/media/index_bg.950b16ee.jpg"
+  },
+  {
+    "revision": "3b7912f1826fdd04bfe0eaa3365b3d61",
+    "url": "/static/media/Concert_bg.3b7912f1.jpg"
+  },
+  {
+    "revision": "8dc2d72fd9fae39d3baaa109bd62e07f",
+    "url": "/index.html"
+  }
+];
